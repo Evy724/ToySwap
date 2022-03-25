1b0e062198ba34e0787e76054eb3890f65013b83
sarahkorrin <sarahkorrin@tutanota.com>
2022-03-25 11:06:02 -0400

    View Item and Finalize Trade Offer API revisions

---
+++ app/src/main/java/com/revature/project2/network/ViewItem_Response_API.kt
@@ -0,0 +1,10 @@
+package com.revature.project2.network
+
+import com.google.gson.annotations.SerializedName
+
+data class ViewItem_Response_API (
+
+    @SerializedName("viewItemRequestTrade_success_msg")
+    val viewItemRequestTrade_success_msg: String,
+
+    )
