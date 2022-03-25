8082c951cc2228b340eb199a61b5bfb99712deed
sarahkorrin <sarahkorrin@tutanota.com>
2022-03-25 05:16:22 -0400

    View Item and Finalize Trade Offer REVISED API's

--- app/src/main/java/com/revature/project2/network/repository/FinalizeTradeOffer_AuthAPIService.kt
+++ app/src/main/java/com/revature/project2/network/repository/FinalizeTradeOffer_AuthAPIService.kt
@@ -4,10 +4,15 @@ import com.revature.project2.network.FinalizeTradeOffer_Request_API
 import com.revature.project2.network.FinalizeTradeOffer_Token_API
 import com.revature.project2.network.ViewItem_Request_API
 import com.revature.project2.network.ViewItem_Token_API
-import okhttp3.Response
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.POST
 
 interface FinalizeTradeOffer_AuthAPIService
 {
-    suspend fun getLogin(@Body finalizeTradeOffer_Request_API: FinalizeTradeOffer_Request_API): Response<FinalizeTradeOffer_Token_API>
+    @POST("finalizeTradeOffer_auth_token")
+    suspend fun getFinalizeTradeOffer_Request_API(
+        @Body finalizeTradeOffer_Request_API: FinalizeTradeOffer_Request_API
+    ):
+            Response<FinalizeTradeOffer_Token_API>
 }
--- app/src/main/java/com/revature/project2/network/repository/ViewItem_AuthAPIService.kt
+++ app/src/main/java/com/revature/project2/network/repository/ViewItem_AuthAPIService.kt
@@ -2,10 +2,15 @@ package com.revature.project2.network.repository
 
 import com.revature.project2.network.ViewItem_Request_API
 import com.revature.project2.network.ViewItem_Token_API
-import okhttp3.Response
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.POST
 
 interface ViewItem_AuthAPIService
 {
-    suspend fun getLogin(@Body viewItem_Request_API: ViewItem_Request_API): Response<ViewItem_Token_API>
+    @POST("viewItem_auth_token")
+    suspend fun getViewItem_Request_API(
+        @Body viewItem_Request_API: ViewItem_Request_API
+    ):
+            Response<ViewItem_Token_API>
 }
