adb6ca3528a9e7cb74e5a4e96f2c28a41a428cb9
sarahkorrin <sarahkorrin@tutanota.com>
2022-03-25 11:09:23 -0400

    View Item and Finalize Trade Offer API revisions

--- app/src/main/java/com/revature/project2/network/ViewItem_Request_API.kt
+++ app/src/main/java/com/revature/project2/network/ViewItem_Request_API.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class ViewItem_Request_API (
 
     @SerializedName("viewItem_toy_id")
-    val viewItem_toy_id: String,
+    val viewItem_toy_id: Int,
 
     @SerializedName("viewItem_toy_type")
     val viewItem_toy_type: String
