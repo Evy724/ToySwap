e12f07a9c117beda5affdb66a1ba7dce97445185
RyanKCox <ryankcox90@gmail.com>
2022-03-26 12:24:47 -0500

    Added Scaffold with top and bottom app bars

--- app/src/main/java/com/revature/project2/view/composables/BrowseItems.kt
+++ app/src/main/java/com/revature/project2/view/composables/BrowseItems.kt
@@ -4,41 +4,53 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.revature.project2.viewmodel.AllToysViewModel
 
+/**
+ * Browse Items screen using scaffold
+ *
+ * Uses ToyCard and BottomBar from shared file
+ */
 @Composable
 fun BrowseItemsScreen(navController: NavController, viewModel:AllToysViewModel){
 
-    val lazyState = rememberLazyListState()
+    val scaffoldState = rememberScaffoldState(
+        rememberDrawerState(DrawerValue.Closed))
 
+    Scaffold(scaffoldState = scaffoldState,
+        topBar = { TopAppBar( title = {Text("Browse Items: ")},
+        backgroundColor = MaterialTheme.colors.secondary) },
+        content = {
 
-    val toyList = viewModel.allToys
+            val lazyState = rememberLazyListState()
+            val toyList = viewModel.allToys
 
-    if (toyList.isNotEmpty())
-    {
-        LazyColumn(state = lazyState,
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally){
+            if (toyList.isNotEmpty())
+            {
+                LazyColumn(state = lazyState,
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally){
 
-                itemsIndexed(toyList) { _, item ->
-                    ToyCard(toy = item)
+                    itemsIndexed(toyList) { _, item ->
+                        ToyCard(toy = item)
+                    }
                 }
             }
-    }
-    else {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("Loading", style = MaterialTheme.typography.h3)
+            else {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Loading", style = MaterialTheme.typography.h3)
 
-        }
-    }
+                }
+            }
+        },
+        bottomBar = { BottomBar() })
 }
--- app/src/main/java/com/revature/project2/view/composables/Shared.kt
+++ app/src/main/java/com/revature/project2/view/composables/Shared.kt
@@ -1,39 +1,115 @@
 package com.revature.project2.view.composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.rememberCoilPainter
 import com.revature.project2.model.api.alltoys.ToyItem
 
 
+/**
+ * Toy Display card
+ *
+ * button functionality not created yet
+ */
 @Composable
 fun ToyCard(toy: ToyItem){
 
-    Row(modifier = Modifier.fillMaxWidth(.95f)) {
+    Card(modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth()
+        .height(100.dp)
+        .wrapContentHeight()
+        .clickable { },
+        shape = MaterialTheme.shapes.medium,
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.surface){
 
-        Image(painter = rememberCoilPainter(
-            request = toy.sImagePath)
-            , contentDescription = null,
-            modifier = Modifier.size(70.dp))
+        Row(verticalAlignment = Alignment.Top) {
 
-        Spacer(Modifier.size(5.dp))
+            Image(painter = rememberCoilPainter(
+                request = toy.sImagePath),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(5.dp),
+                contentScale = ContentScale.Fit)
 
-        Column {
+            Spacer(Modifier.size(5.dp))
 
-            Text(toy.sName,
-                style = MaterialTheme.typography.subtitle1,
-                textAlign = TextAlign.Center)
+            Column( modifier = Modifier
+                .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
 
-            Spacer(Modifier.size(2.dp))
+                Text(toy.sName,
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Center)
 
-            Text(text = toy.sDescription,
-                style = MaterialTheme.typography.body1)
+                Spacer(Modifier.size(2.dp))
+
+                Text(text = toy.sDescription,
+                    style = MaterialTheme.typography.body1)
+            }
         }
+
+    }
+}
+
+/**
+ * Bottom app bar for use between screens
+ *
+ * button functionality not created yet
+ */
+@Composable
+fun BottomBar(){
+    
+    val selectedIndex = rememberSaveable { mutableStateOf(0) }
+    BottomNavigation(
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.secondary) {
+        
+        BottomNavigationItem(
+            selected = selectedIndex.value == 0,
+            onClick = {
+                selectedIndex.value = 0 },
+            icon = { Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "")},
+            label = {Text("Browse")}
+            )
+
+        BottomNavigationItem(
+            selected = selectedIndex.value == 1,
+            onClick = {
+                selectedIndex.value = 1 },
+            icon = { Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "")},
+            label = {Text("Post")}
+        )
+
+        BottomNavigationItem(
+            selected = selectedIndex.value == 2,
+            onClick = {
+                selectedIndex.value = 2 },
+            icon = { Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "")},
+            label = {Text("Profile")}
+        )
+
     }
 }
