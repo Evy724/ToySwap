8dc8436cb45124895ac13e2839d10544f4d08c20
jacobedens <100707183+jacobedens@users.noreply.github.com>
2022-04-07 21:51:17 -0500

    Added to Nav

--- app/src/main/java/com/revature/project2/view/composables/NewToyPostActivity.kt
+++ app/src/main/java/com/revature/project2/view/composables/NewToyPostActivity.kt
@@ -1,24 +1,33 @@
 package com.revature.project2.view.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
-/*
+
 
 @Composable
-fun newToyPostScreen(navController: NavController){
+fun NewToyPostScreen(navController: NavController){
 Scaffold(bottomBar = {BottomBar(navController)}) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .background(
+                MaterialTheme.colors.surface,
+                RectangleShape
+            )
+            .fillMaxWidth()
     )
     {
 
@@ -26,8 +35,8 @@ Scaffold(bottomBar = {BottomBar(navController)}) {
         inputToyName()
         toyDescription()
         ageOfToy()
-        postToy()
+        postToy(navController)
     }
 }
 
-}*/
+}
