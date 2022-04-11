ccf078573c550df7de7b97b653728398c014a56d
jacobedens <100707183+jacobedens@users.noreply.github.com>
2022-04-11 08:15:29 -0500

    Added to Nav

--- app/src/main/java/com/revature/project2/view/composables/NewToyPost.kt
+++ app/src/main/java/com/revature/project2/view/composables/NewToyPost.kt
@@ -1,20 +1,32 @@
 package com.revature.project2.view.composables
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.revature.project2.R
+import com.revature.project2.ui.theme.BluishGreen
+import com.revature.project2.ui.theme.PurpleVariant
 import com.revature.project2.view.nav.NavScreens
+import com.revature.project2.viewmodel.AllToysViewModel
+import java.time.format.TextStyle
 
 //@Composable
 //fun postNewToy(){
@@ -28,27 +40,50 @@ import com.revature.project2.view.nav.NavScreens
 //        onClick = { println("Intent to img upload prompt") })
 
 @Composable
-fun newToyPostScreen(navController: NavController){
-    Scaffold(bottomBar = {BottomBar(navController)}) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        )
+fun newToyPostScreen(navController: NavController) {
+    Scaffold(bottomBar = { BottomBar(navController) })
+    {
+        Surface()
         {
+            Image(
+                painter = painterResource(id = R.drawable.minimal_blue_toy_background),
+                contentDescription = "he",
+                modifier = Modifier
+                    .height(1000.dp)
+                    .width(700.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillBounds
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+//                    .background(
+//
+//                        brush = Brush.horizontalGradient(
+//                            colors = listOf(
+//                                PurpleVariant,
+//                                BluishGreen
+//                            )
+//                        )
+//                    )
+            )
+            {
 
-            screenTitle()
-            inputToyName()
-            toyDescription()
-            ageOfToy()
-            postToy(navController)
+                screenTitle()
+                inputToyName()
+                toyDescription()
+                ageOfToy()
+                postToy(navController)
+            }
         }
     }
 
 }
 @Composable
 fun screenTitle() {
-    Text("New Toy Post", fontSize = 40.sp, modifier = Modifier
+    Text("New Toy Post", fontSize = 40.sp,color = Color.White, modifier = Modifier
         .padding(10.dp)
         .wrapContentHeight(), textAlign = TextAlign.Center)
 }
@@ -107,5 +142,10 @@ fun postToy(navController: NavController)
             navController.popBackStack(
                 NavScreens.BrowseItemsScreen.route,false
             ) },
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .height(100.dp)
+            .wrapContentHeight(),
     )
 }
